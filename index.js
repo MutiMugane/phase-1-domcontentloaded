942acb606579509af902f053512b78609d28d2c4
+ <+>
2022-08-31 08:21:12 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     console.log("The DOM has loaded");
+//   });
+  function change_text(){
+  document.getElementById("text").innerHTML = "This is really cool!";
+  }
+window.onload=change_text();
